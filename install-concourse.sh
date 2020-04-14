77bb5ac51cf78c00744e02c34305265351902c3a
Jamie Kelly <git@jamiekelly.com>
2020-04-14 21:44:41 +0100

    AMI with keys

--- install-concourse.sh
+++ install-concourse.sh
@@ -1,10 +1,19 @@
-# Get the concourse tarball
+# Install Postgresql
+sudo amazon-linux-extras install postgresql10 vim epel -y
+sudo yum install -y postgresql-server
+sudo /usr/bin/postgresql-setup --initdb
+sudo su postgres -c "createuser ec2-user"
+sudo su postgres -c "createdb --owner=ec2-user atc"
+sudo service postgresql enable
+sudo service postgresql start
+
+# Install Concourse
 wget https://github.com/concourse/concourse/releases/download/v6.0.0/concourse-6.0.0-linux-amd64.tgz -O concourse.tgz
-
-# Untar
 sudo tar -C /usr/local -xzf concourse.tgz
-
-# Remove tarball
 rm concourse.tgz
 
-concourse
+# Generate keys
+/usr/local/bin/concourse generate-key -t rsa -f /home/ec2-user/session_signing_key
+/usr/local/bin/concourse generate-key -t ssh -f /home/ec2-user/tsa_host_key
+/usr/local/bin/concourse generate-key -t ssh -f /home/ec2-user/worker_key
+cp /home/ec2-user/worker_key /home/ec2/user/authorized_worker_keys
