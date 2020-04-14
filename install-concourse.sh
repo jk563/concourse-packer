9f6f9f1349e64ee97ec76f1d1ecf49fb93d2870a
Jamie Kelly <git@jamiekelly.com>
2020-04-14 21:35:04 +0100

    Use sudo for installation

--- install-concourse.sh
+++ install-concourse.sh
@@ -2,7 +2,7 @@
 wget https://github.com/concourse/concourse/releases/download/v6.0.0/concourse-6.0.0-linux-amd64.tgz -O concourse.tgz
 
 # Untar
-tar -C /usr/local -xzf concourse.tgz
+sudo tar -C /usr/local -xzf concourse.tgz
 
 # Remove tarball
 rm concourse.tgz
