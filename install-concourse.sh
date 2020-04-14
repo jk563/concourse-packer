9f5fc724bed23e352d84a0edf046580194382d01
Jamie Kelly <git@jamiekelly.com>
2020-04-14 22:59:50 +0100

    Add worker service

--- install-concourse.sh
+++ install-concourse.sh
@@ -30,13 +30,32 @@ Type=simple
 Restart=always
 RestartSec=1
 User=ec2-user
-ExecStart=/usr/local/concourse/bin/concourse web
+ExecStart=/usr/local/concourse/bin/concourse web --add-local-user jamie:norbit --main-team-local-user jamie --session-signing-key /home/ec2-user/session_signing_key --tsa-host-key /home/ec2-user/tsa_host_key --tsa-authorized-keys /home/ec2-user/authorized_worker_keys --postgres-user ec2-user --postgres-socket /var/run/postgresql --external-url=http://ci.jamiekelly.com:8080
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Create worker service unit
+sudo cat <<EOF > concourse-worker.service 
+[Unit]
+Description=Concourse Worker Agent
+After=cooncourse-web.service
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=ec2-user
+ExecStart=/usr/local/concourse/bin/concourse worker --work-dir /opt/concourse/worker --tsa-host 127.0.0.1:2222 --tsa-public-key /home/ec2-user/tsa_host_key.pub --tsa-worker-private-key /home/ec2-user/worker_key
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
 sudo mv concourse-web.service /etc/systemd/system/concourse-web.service
+sudo mv concourse-worker.service /etc/systemd/system/concourse-worker.service
 
 sudo systemctl enable concourse-web
-sudo service concourse-web start
+sudo systemctl enable concourse-worker
