b1eb91d4fdfc62ded84a23fa45653584cd22cead
Jamie Kelly <git@jamiekelly.com>
2020-04-18 15:00:23 +0100

    Check for images in London

--- fail-if-exists.sh
+++ fail-if-exists.sh
@@ -2,7 +2,7 @@
 
 COMMIT_ID=${1}
 
-IMAGES_WITH_COMMIT_ID=$(aws ec2 describe-images --filters "Name=tag:commit,Values=${COMMIT_ID}" | jq '.Images | length')
+IMAGES_WITH_COMMIT_ID=$(aws ec2 describe-images --filters "Name=tag:commit,Values=${COMMIT_ID}" --region eu-west-2 | jq '.Images | length')
 
 if test ${IMAGES_WITH_COMMIT_ID} != 0; then
 	exit 1
